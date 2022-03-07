039c676b3e701d535173de5a97a8ae5b1be648d8
SebastianCatur <vasile-sebastian.catur@endava.com>
2022-03-07 13:29:39 +0200

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Vasile Sebastian Catur on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
